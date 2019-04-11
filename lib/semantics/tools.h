669b05b27d7ad5bee6689507d40199e941838b9a
Paul Osmialowski <pawel.osmialowski@arm.com>
2019-04-11 21:25:45 +0100

    Semantics checker for STOP and ERROR STOP statements - ExprTypeKindIsDefault added to the tools
    Signed-off-by: Paul Osmialowski <pawel.osmialowski@arm.com>

--- lib/semantics/tools.h
+++ lib/semantics/tools.h
@@ -19,6 +19,7 @@
 // canonically for use in semantic checking.
 
 #include "scope.h"
+#include "semantics.h"
 #include "symbol.h"
 #include "type.h"
 #include "../evaluate/variable.h"
@@ -84,6 +85,8 @@ const Symbol *FindExternallyVisibleObject(
 bool ExprHasTypeCategory(
     const evaluate::GenericExprWrapper &expr, const common::TypeCategory &type);
 bool ExprHasTypeKind(const evaluate::GenericExprWrapper &expr, int kind);
+bool ExprTypeKindIsDefault(
+    const evaluate::GenericExprWrapper &expr, const SemanticsContext &context);
 bool ExprIsScalar(const evaluate::GenericExprWrapper &expr);
 void CheckScalarLogicalExpr(
     const parser::Expr &expr, parser::Messages &messages);
