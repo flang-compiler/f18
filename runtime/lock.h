7047e4a1cd340bc7a6bd2cb10f1a9bdf0de7968f
Isuru Fernando <isuruf@gmail.com>
2020-02-20 14:37:02 -0600

    Use SRWLOCK on windows

--- runtime/lock.h
+++ runtime/lock.h
@@ -11,17 +11,29 @@
 #ifndef FORTRAN_RUNTIME_LOCK_H_
 #define FORTRAN_RUNTIME_LOCK_H_
 
+#ifdef _MSC_VER
+#include <synchapi.h>
+#else
 #include <pthread.h>
+#endif
 
 namespace Fortran::runtime {
 
 class Lock {
 public:
+#ifdef _MSC_VER
+  Lock() { InitializeSRWLock(&mutex_); }
+  ~Lock() { }
+  void Take() { AcquireSRWLockExclusive(&mutex_); }
+  bool Try() { return TryAcquireSRWLockExclusive(&mutex) != 0; }
+  void Drop() { ReleaseSRWLockExclusive(&mutex_); }
+#else
   Lock() { pthread_mutex_init(&mutex_, nullptr); }
   ~Lock() { pthread_mutex_destroy(&mutex_); }
   void Take() { pthread_mutex_lock(&mutex_); }
   bool Try() { return pthread_mutex_trylock(&mutex_) != 0; }
   void Drop() { pthread_mutex_unlock(&mutex_); }
+#endif
 
   void CheckLocked(const Terminator &terminator) {
     if (Try()) {
@@ -31,7 +43,11 @@ public:
   }
 
 private:
+#ifdef _MSC_VER
+  SRWLOCK mutex_;
+#else
   pthread_mutex_t mutex_;
+#endif
 };
 
 class CriticalSection {
