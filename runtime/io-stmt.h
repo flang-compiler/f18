14f7ed6b66904e0f6345a43a78ee596b28805bdc
Patrick McCormick <pat@lanl.gov>
2020-02-25 11:37:16 -0700

    Clean up merge issues.

--- runtime/io-stmt.h
+++ runtime/io-stmt.h
@@ -30,6 +30,7 @@ protected:
 class InternalIoStatementState : public IoStatementState {
 public:
   InternalIoStatementState(const char *sourceFile, int sourceLine);
+  virtual ~InternalIoStatementState() {}
   virtual int EndIoStatement();
 
 protected:
