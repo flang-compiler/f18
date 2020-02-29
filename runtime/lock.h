0b0c8955903a2d96baf4241bf115ae53230c5c0b
Isuru Fernando <isuruf@gmail.com>
2020-02-28 18:10:25 -0600

    Use std::mutex

--- runtime/lock.h
+++ runtime/lock.h
@@ -11,44 +11,23 @@
 #ifndef FORTRAN_RUNTIME_LOCK_H_
 #define FORTRAN_RUNTIME_LOCK_H_
 
-#ifdef _MSC_VER
-#define NOMINMAX
-#include <windows.h>
-#else
-#include <pthread.h>
-#endif
+#include <mutex>
 
 namespace Fortran::runtime {
 
 class Lock {
 public:
-#ifdef _MSC_VER
-  Lock() { InitializeSRWLock(&mutex_); }
-  ~Lock() { }
-  void Take() { AcquireSRWLockExclusive(&mutex_); }
-  bool Try() { return TryAcquireSRWLockExclusive(&mutex) != 0; }
-  void Drop() { ReleaseSRWLockExclusive(&mutex_); }
-#else
-  Lock() { pthread_mutex_init(&mutex_, nullptr); }
-  ~Lock() { pthread_mutex_destroy(&mutex_); }
-  void Take() { pthread_mutex_lock(&mutex_); }
-  bool Try() { return pthread_mutex_trylock(&mutex_) != 0; }
-  void Drop() { pthread_mutex_unlock(&mutex_); }
-#endif
-
+  void Take() { mutex_.lock(); }
+  bool Try() { return mutex_.try_lock(); }
+  void Drop() { mutex_.unlock(); }
   void CheckLocked(const Terminator &terminator) {
     if (Try()) {
       Drop();
       terminator.Crash("Lock::CheckLocked() failed");
     }
   }
-
 private:
-#ifdef _MSC_VER
-  SRWLOCK mutex_;
-#else
-  pthread_mutex_t mutex_;
-#endif
+  std::mutex mutex_;
 };
 
 class CriticalSection {
